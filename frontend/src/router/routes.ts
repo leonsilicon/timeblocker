d68e4dab1cd84bc0d5d7a2a677a8cc44c7304e98
Leon Si <contact@leonzalion.com>
2022-02-01 15:40:00 -0500

    fix: router

--- frontend/src/router/routes.ts
+++ frontend/src/router/routes.ts
@@ -1,5 +1,12 @@
 import type { RouteRecordRaw } from 'vue-router';
 
-export const routes: RouteRecordRaw[] = [{
-	path: '/'
-}];
+export const routes: RouteRecordRaw[] = [
+	{
+		path: '/',
+		component: async () => import('~/pages/landing-page.vue'),
+	},
+	{
+		path: ':catchAll(.*)',
+		component: async () => import('~/pages/404-page.vue'),
+	},
+];
