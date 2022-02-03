2d25c9cd3079f3f2234888004805df9fd507b670
Leon Si <contact@leonzalion.com>
2022-02-02 21:01:46 -0500

    chore: upgrade deps

--- frontend/src/components/timeblock-page-task-dock.vue
+++ frontend/src/components/timeblock-page-task-dock.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-import { useAppStore } from '~f/store/use.js';
+import { useTimeblockStore } from '~f/store';
 
-const appStore = useAppStore();
+const timeblockStore = useTimeblockStore();
 </script>
 
 <template>
