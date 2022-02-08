d3343422e7176525493cf78227879327ce381d0f
Leon Si <contact@leonzalion.com>
2022-02-08 14:59:51 -0500

    feat: changes

--- backend/scripts/build.ts
+++ backend/scripts/build.ts
@@ -1,9 +1,11 @@
 import process from 'node:process';
 import { execaCommandSync as exec } from 'execa';
+import { copyPackageFiles} from 'lion-system'
 
 try {
 	exec('tsc', { stdio: 'inherit' });
 	exec('tsc-alias', { stdio: 'inherit' });
+	copyPackageFiles();
 } catch {
 	process.exit(1);
 }
