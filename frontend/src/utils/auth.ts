4a9d76167fe76e269f6c9a1a20cbfa482d643dde
Leon Si <contact@leonzalion.com>
2022-02-19 11:51:59 -0500

    chore: removed unused code

--- frontend/src/utils/auth.ts
+++ frontend/src/utils/auth.ts
@@ -1,5 +1,4 @@
 import { LocalStorageKey } from '~f/types/local-storage';
-import { AuthenticationMethod } from '~s/types/auth';
 
 export function authenticateUser(props: { sessionToken: string }) {
 	window.localStorage.setItem(LocalStorageKey.sessionToken, props.sessionToken);
